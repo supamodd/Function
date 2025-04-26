8c231dc863d45d584770c6eeb31e1f8e108521b7
VLAD <VLAD@DESKTOP-QBN74TL>
2025-04-26 11:31:40 +0300

    StatLIB DONE

---
+++ LibChekk/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<FillRand.h>
+#include<Print.h>
+#include<sort.h>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+
+	cout << delimiter << endl;
+
+	int i_arr_2[ROWS][COLS];
+	FillRand(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);
+}
